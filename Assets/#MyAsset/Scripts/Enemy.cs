ecd0fedd2630d3778feeff70e01018e23eaf15e2
DESKTOP-U4EQ6CT\梅川 俊樹 <mahouka.sao.kinsho315@gamil>
2018-03-18 22:29:20 +0900

    検知完成

--- Assets/#MyAsset/Scripts/Enemy.cs
+++ Assets/#MyAsset/Scripts/Enemy.cs
@@ -2,20 +2,41 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// SearchingBehaviorを使用するスクリプト
+// SearchingBehaviorで何かを見つけたら色が変化
 public class Enemy : MonoBehaviour {
+    [SerializeField]
+    private Material m_defaultMaterial = null;
+    [SerializeField]
+    private Material m_foundMaterial = null;
+        
+    // マテリアルをいじっている？
+    private Renderer m_renderer = null;
+    private List<GameObject> m_targets = new List<GameObject>();
 
-    // プレイヤーを見つけているかどうか
-    private bool find;
 
+    private void Awake()
+    {
+        m_renderer = GetComponentInChildren<Renderer>();
 
+        var searching = GetComponentInChildren<SearchingBehavior>();
+        searching.OnFound += OnFound;
+        searching.OnLost += OnLost;
+    }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private void OnFound(GameObject i_foundObject)
+    {
+        m_targets.Add(i_foundObject);
+        m_renderer.material = m_foundMaterial;
+    }
+
+    private void OnLost(GameObject i_lostObject)
+    {
+        m_targets.Remove(i_lostObject);
+
+        if (m_targets.Count == 0)
+        {
+            m_renderer.material = m_defaultMaterial;
+        }
+    }
 }
--- Assets/#MyAsset/Scripts/Search/SearchingBehavior.cs
+++ Assets/#MyAsset/Scripts/Search/SearchingBehavior.cs
@@ -2,8 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-//索敵処理を記述するスクリプト
+// 索敵処理を記述するスクリプト
 public class SearchingBehavior : MonoBehaviour {
 
+    // 対象者を見つけたり見失ったりしたら、イベントを返す
+    public event System.Action<GameObject> OnFound = (obj) => { };
+    public event System.Action<GameObject> OnLost = (obj) => { };
 
+    // SphereColliderに触れた時
+    private void OnTriggerEnter(Collider i_other)
+    {
+        GameObject enterObject = i_other.gameObject;
+        OnFound(enterObject);
+    }
+
+    // SphereColliderから離れた時
+    private void OnTriggerExit(Collider i_other)
+    {
+        GameObject exitObject = i_other.gameObject;
+        OnLost(exitObject);
+    }
 }
